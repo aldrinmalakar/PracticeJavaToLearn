ec8263dbb5fbe0f52edfaa9230bdd058b0041e3b
aldrinmalakar <aldrinmalakar@gmail.com>
2020-09-16 01:19:54 -0400

    Version 2

--- src/javaPractice/practiceForLoop.java
+++ src/javaPractice/practiceForLoop.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class practiceForLoop {
 
     public practiceForLoop (){
-
+        for (int y=0; y<10;y++){
         Scanner j = new Scanner(System.in);
         System.out.println("Enter number of your choice");
         int i = j.nextInt();
 
-        if (i == 5 || i == 10) {
-            System.out.println("So far we are in the if loop");
+        if (i<=10){
+                System.out.println("So far you are about 40 away from the right number");
+            } else if (i==50){
+                System.out.println("Uhh there you are!");
         } else {
-            System.out.println("Nah bro, we din't make it!");
-
+            System.out.println("Bitch get a life!");
+        } break;
         }
 
     }
