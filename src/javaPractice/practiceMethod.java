2f20518565469ff7844c0068638a65236621da08
Aldrin <aldrinmalakar@gmail.com>
2020-09-26 15:26:12 -0400

    After Practice

---
+++ src/javaPractice/practiceMethod.java
@@ -0,0 +1,16 @@
+package javaPractice;
+
+public class practiceMethod {
+	
+	public static int methodAddition(int num1, int num2) {
+		
+	return num1+num2;		
+	}
+	
+	
+	public static void main(String[] args) {
+		methodAddition(2,5);
+	}
+	
+
+}
