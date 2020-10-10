3964ef4fc62681f4096d0e80144588b3f9381051
Aldrin <aldrinmalakar@gmail.com>
2020-10-09 23:48:47 -0400

    End of night

---
+++ src/javaPractice/arrayListPractice/Array2DOutput.java
@@ -0,0 +1,27 @@
+package javaPractice.arrayListPractice;
+
+import java.util.Scanner;
+
+public class Array2DOutput {
+public static void main(String[] args) {
+	notMain();
+	
+}
+
+private static void notMain() {
+	Scanner input = new Scanner(System.in);
+	int[][] A = new int[2][3];
+	
+
+	// Getting user input for A 2D Array
+	System.out.println("Enter 6 numerical values for the list A");
+	for (int row = 0; row < 2; row++) {
+
+		for (int col = 0; col < 3; col++) {
+			System.out.printf("A [%d][%d]=", row, col);
+			A[row][col] = input.nextInt();
+		}
+
+	}
+}
+}
