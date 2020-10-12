b8379e4c9f328067c8f770446aa2a29b9af8b536
aldrinmalakar <aldrinmalakar@gmail.com>
2020-10-12 11:47:33 -0400

    Update morning

---
+++ src/javaPractice/Array/Practice2DArrayMonday.java
@@ -0,0 +1,21 @@
+package javaPractice.Array;
+
+public class Practice2DArrayMonday {
+
+    public static void main(String[] args) {
+        separateMethod();
+        Scanner
+    }
+
+    public static void separateMethod() {
+        Scanner input = new Scanner(System.in);
+        int [][] arrayVar = new int[3] [3];
+
+        for (int row = 0; row < 3; row++){
+            for (int col = 0; col < 3; col++){
+                System.out.println("");
+                arrayVar[row][col] =
+            }
+        }
+    }
+}
